22c7bd0995f0a49b409ad003e4060438e03e6e67
woodland021 <ethan021@me.com>
2016-09-10 03:05:39 -0700

    Initial Commit

---
+++ WeatherChecker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherChecker
+//
+//  Created by Ethan Thomas on 9/10/16.
+//  Copyright © 2016 Ethan Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
